1f894b9c4f4ddaa756bdc2e9d08487728e4a3f96
Jens L. Hammer <61454021+JLHammer@users.noreply.github.com>
2026-03-17 12:59:30 +0100

    Remove rejseplanenModule from index.js
    Removed import and usage of rejseplanenModule.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import {get} from "./utils/get"
 import {set} from "./utils/set"
-import { rejseplanenModule } from "./modules/RejseplanenModule";
 
 const app = get("#app")
 
-const d = rejseplanenModule()
-
-set(d, app)
